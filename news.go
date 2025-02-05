672ff66d1bd4c7cda3364c981aef2a254db606e6
Daniel Vieites <dvieitest@gmail.com>
2025-02-05 10:10:10 +0100

    feat: can choose and select a category

--- news.go
+++ news.go
@@ -4,13 +4,24 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type News struct {
+type AppStatus int
 
+const (
+	Principal AppStatus = iota
+)
+
+type News struct {
+	Handlers map[AppStatus]tea.Model
+	Status   AppStatus
 }
 
 func NewNews() News {
-	return News{
+	h := make(map[AppStatus]tea.Model)
+	h[Principal] = NewPrincipal()
 
+	return News{
+		Handlers: h,
+		Status:   Principal,
 	}
 }
 
@@ -19,9 +30,22 @@ func (n News) Init() tea.Cmd {
 }
 
 func (n News) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return n, nil
+	currentStatus := n.Status
+	// Global key mappings.
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return n, tea.Quit
+		}
+	}
+
+	// Per handler key mappings.
+	updated, cmd := n.Handlers[n.Status].Update(msg)
+	n.Handlers[currentStatus] = updated
+	return n, cmd
 }
 
 func (n News) View() string {
-	return "Hello world!"
+	return n.Handlers[n.Status].View()
 }
---
+++ principal.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	listHeight   = 23
+	defaultWidth = 30
+)
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type category string
+
+func (c category) FilterValue() string { return string(c) }
+
+type categoryDelegate struct{}
+
+func (d categoryDelegate) Height() int                             { return 1 }
+func (d categoryDelegate) Spacing() int                            { return 0 }
+func (d categoryDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d categoryDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(category)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+type PrincipalHandler struct {
+	list   list.Model
+	chosen string
+}
+
+func NewPrincipal() PrincipalHandler {
+	categories := []list.Item{
+		category("Business"),
+		category("Crime"),
+		category("Domestic"),
+		category("Education"),
+		category("Entertaiment"),
+		category("Environment"),
+		category("Food"),
+		category("Health"),
+		category("Lifestyle"),
+		category("Other"),
+		category("Politics"),
+		category("Science"),
+		category("Sports"),
+		category("Technology"),
+		category("Top"),
+		category("Tourism"),
+		category("World"),
+	}
+
+	l := list.New(categories, categoryDelegate{}, defaultWidth, listHeight)
+	l.Title = "Choose a category."
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(true)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
+	return PrincipalHandler{
+		list: l,
+	}
+}
+
+func (p PrincipalHandler) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "enter":
+			p.chosen = p.list.SelectedItem().FilterValue()
+			return p, nil
+		}
+	}
+	var cmd tea.Cmd
+	p.list, cmd = p.list.Update(msg)
+	return p, cmd
+}
+
+func (p PrincipalHandler) Init() tea.Cmd {
+	return nil
+}
+
+func (p PrincipalHandler) View() string {
+	s := ""
+
+	if p.chosen != "" {
+		s += fmt.Sprintf("You have chosen %s", p.chosen)
+	}
+
+	s += "\n" + p.list.View()
+
+	return s
+}
