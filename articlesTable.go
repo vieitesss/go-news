fd1e57644ee647b28ed140cd83f56f136b39bcae
Daniel Vieites <dvieitest@gmail.com>
2025-02-07 10:43:13 +0100

    feat: spinner while searching for articles

--- articlesTable.go
+++ articlesTable.go
@@ -3,19 +3,29 @@ package main
 import (
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/vieitesss/go-news/api"
 )
 
 type MsgGetByCategoryResponse api.ApiResponse
 
 type ArticlesTableHandler struct {
+	spinner  spinner.Model
+	waiting  bool
 	Category string
 	Response api.ApiResponse
 }
 
 func NewArticlesTable(category string) ArticlesTableHandler {
+	s := spinner.New()
+	s.Spinner = spinner.Meter
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
 	return ArticlesTableHandler{
+		spinner:  s,
+		waiting:  true,
 		Category: category,
 	}
 }
@@ -27,17 +37,31 @@ func GetArticlesByCategory(category string) tea.Cmd {
 }
 
 func (a ArticlesTableHandler) Init() tea.Cmd {
-	return GetArticlesByCategory(a.Category)
+	return tea.Batch(
+		GetArticlesByCategory(a.Category),
+		a.spinner.Tick,
+	)
 }
 
 func (a ArticlesTableHandler) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case MsgGetByCategoryResponse:
+		a.waiting = false
 		a.Response = api.ApiResponse(msg)
+
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		a.spinner, cmd = a.spinner.Update(msg)
+		return a, cmd
 	}
+
 	return a, nil
 }
 
 func (a ArticlesTableHandler) View() string {
-	return fmt.Sprintf("%+v", a)
+	if a.waiting {
+		return fmt.Sprintf("\n    %s", a.spinner.View())
+	}
+
+	return fmt.Sprintf("%+v", a.Response)
 }
